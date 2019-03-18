33acf7b288766b9f7d343cf404ae8846803f67f4
Moritz Gehlhaar <moritz.gehlhaar@posteo.de>
2019-03-18 15:58:01 +0100

    small adjustments to GUI

--- src/SimulationGUI.java
+++ src/SimulationGUI.java
@@ -2,17 +2,9 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.scene.*;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.DrawMode;
 import javafx.scene.shape.Sphere;
-import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -22,6 +14,8 @@ import java.util.Set;
 
 public class SimulationGUI extends Application {
     private PerspectiveCamera camera;
+    //private Label label;
+    private Group labelGroup;
 
     public Parent createContent() throws Exception {
         double auToM = 1.496e11;
@@ -29,7 +23,7 @@ public class SimulationGUI extends Application {
         Planet earth = new Planet(new Vector(-9.918696803493554E-01 * auToM, 9.679454643549934E-02 * auToM, -4.277240997129137E-05 * auToM),
                 new Vector(-1.825836604899280E-03 * auToM * dayToSecond, -1.719621912926312E-02 * auToM * dayToSecond, 3.421794164900239E-07 * auToM * dayToSecond),
                 5.9721986e24,
-                6371008,
+                6371008, // 6.957e9
                 "planettextures/8k_earth_daymap.jpg");
 
         Planet sun = new Planet(new Vector(-1.351343105506232E-03 * auToM, 7.549817138203992E-03 * auToM, -4.200718115315673E-05 * auToM),
@@ -45,25 +39,13 @@ public class SimulationGUI extends Application {
 
         System.out.println(bodies);
 
-        final Timeline timeline = new Timeline(
-                new KeyFrame(
-                        Duration.millis( 5 ),
-                        event -> {
-                            bodies.iterate(0.5);
-
-                            System.out.println(bodies);
-                        }
-                )
-        );
-        timeline.setCycleCount( Animation.INDEFINITE );
-        timeline.play();
-
         Sphere earthSphere = earth.getSphere();
         Sphere sunSphere = sun.getSphere();
 
         // Create and position camera
         camera = new PerspectiveCamera(true);
         camera.getTransforms().addAll(new Translate(earthSphere.getTranslateX(), earthSphere.getTranslateY(), earthSphere.getTranslateZ() - earthSphere.getRadius() * 5));
+        // camera.getTransforms().addAll(new Translate(sunSphere.getTranslateX(), sunSphere.getTranslateY(), sunSphere.getTranslateZ() - sunSphere.getRadius() - 1.489e11));
         camera.setFarClip(1e100);
         camera.setNearClip(1);
 
@@ -73,12 +55,24 @@ public class SimulationGUI extends Application {
         root.getChildren().add(earthSphere);
         root.getChildren().add(sunSphere);
 
+        final Timeline timeline = new Timeline(
+                new KeyFrame(
+                        Duration.millis( 5 ),
+                        event -> {
+                            bodies.iterate(100*100);
+                            // camera.getTransforms().addAll(new Translate(camera.getTranslateX(), camera.getTranslateY(), camera.getTranslateZ() - sunSphere.getRadius() ));
+
+                            // System.out.println(bodies);
+                        }
+                )
+        );
+        timeline.setCycleCount( Animation.INDEFINITE );
+        timeline.play();
+
         // Use a SubScene
-        SubScene subScene = new SubScene(root, 500, 500);
-        subScene.setFill(Color.ALICEBLUE);
+        SubScene subScene = new SubScene(root, 1000, 1000);
+        subScene.setFill(Color.WHITE);
         subScene.setCamera(camera);
-        subScene.addEventHandler(KeyEvent.ANY, event -> System.out.println("Hey"));
-        subScene.setOnKeyPressed(event -> System.out.println("HeyR2"));
         Group group = new Group();
         group.getChildren().add(subScene);
         return group;
