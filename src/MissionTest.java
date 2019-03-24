0c07023c9b8bb86be28b604322f1bf2e46f2a273
Tomasz Darmetko <isinlor@gmail.com>
2019-03-24 20:39:20 +0100

    Load bodies from planets.txt file.

--- src/MissionTest.java
+++ src/MissionTest.java
@@ -10,42 +10,7 @@ public class MissionTest {
      */
     public static void main(String[] args) throws Exception {
 
-//        Bodies bodies = new Bodies<>();
-//
-//        double auToM = 1.496e11;
-//        double dayToSecond = 1.0 / 86400.0;
-//
-//        // HORIZONS data for the 16th of March 2019 00:00 with SSB as origin
-//        bodies.addBody(new Body(
-//            "sun",
-//            new Vector(-1.351343105506232E-03*auToM, 7.549817138203992E-03*auToM, -4.200718115315673E-05*auToM),
-//            new Vector(-8.222950279730839E-06*auToM*dayToSecond, 1.252598675779703E-06*auToM*dayToSecond, 2.140020605610505E-07*auToM*dayToSecond),
-//            1.988435e30,
-//            new Metadata(Color.yellow)
-//        ));
-//
-//        bodies.addBody(new Body(
-//            "earth",
-//            new Vector(-9.918696803493554E-01*auToM, 9.679454643549934E-02*auToM, -4.277240997129137E-05*auToM),
-//            new Vector(-1.825836604899280E-03*auToM*dayToSecond, -1.719621912926312E-02*auToM*dayToSecond, 3.421794164900239E-07*auToM*dayToSecond),
-//            5.9721986e24,
-//            new Metadata(Color.blue)
-//        ));
-//
-//        bodies.addBody(new Body(
-//            "mars",
-//            new Vector(2.341284054032922E-01*auToM,  1.537313782783677E+00*auToM, 2.623394307816976E-02*auToM),
-//            new Vector(-1.331027418143195E-02*auToM*dayToSecond,  3.319493802125395E-03*auToM*dayToSecond, 3.961351541925593E-04*auToM*dayToSecond),
-//            6.41693e23,
-//            new Metadata(Color.red)
-//        ));
-//
-////        System.out.println(getMinDistance(bodies.copy(), "mars"));
-////        animate(bodies.copy());
-//
-////        display(bodies);
-
-        optimize(new Bodies(CSVReader.readPlanets()));
+        optimize(Bodies.unserialize(FileSystem.tryLoadResource("planets.txt")));
 
     }
 
