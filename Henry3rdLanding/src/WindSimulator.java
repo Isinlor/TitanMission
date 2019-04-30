4443bb1b4ee7477e56c7ff06dff216d831ddccb6
sharelove <ruben.c.havens@gmail.com>
2019-04-30 12:56:59 +0200

    Wind simulator

--- Henry3rdLanding/src/WindSimulator.java
+++ Henry3rdLanding/src/WindSimulator.java
@@ -2,14 +2,29 @@ import java.util.Random;
 
 public class WindSimulator {
 
-    private Force wind;
-
     public static Force getWind(){
         Random generator=new Random();
-        double x = generator.nextInt(72);
-        double y = generator.nextInt(72);
+        double random = generator.nextInt(10);
+        double x=0;
+        double y=0;
+
+        System.out.println(random);
+
+        if (random<5){
+            x = generator.nextInt(30);
+            y = generator.nextInt(30);
+        }
+        else if(random>=5 && random<8){
+            x = generator.nextInt(50);
+            y = generator.nextInt(50);
+        }
+        else {
+            x = generator.nextInt(72);
+            y = generator.nextInt(72);
+        }
+
         Force wind = new Force(x,y);
-        System.out.println("Wind force: " + wind.toString() + "Length: " + wind.getLength());
+        System.out.println("Wind force: " + wind.toString() + "\nLength: " + wind.getLength());
         return wind;
     }
 
