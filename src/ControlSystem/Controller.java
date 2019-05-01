68f990f20fc792b41789d3a2dffbba2e7a546053
Tomasz Darmetko <isinlor@gmail.com>
2019-05-01 17:23:38 +0200

    Small documentation update.

--- src/ControlSystem/Controller.java
+++ src/ControlSystem/Controller.java
@@ -8,7 +8,8 @@ import Simulation.Spacecraft;
  * The implementing controller can be either open loop or closed loop.
  * https://en.wikipedia.org/wiki/Control_theory#Open-loop_and_closed-loop_(feedback)_control
  *
- * Spaceship is given as an argument the spaceship in order to reduce complexity that would be related to
+ * Why are open and closed loop controllers not separate?
+ * Spaceship is given as an argument the controller in order to reduce complexity that would be related to
  * differentiating between open and closed loop controllers. The differentiation would require two interfaces and two
  * implementations of spacecrafts that take controller as an argument.
  */
