e9a5a43f036919147354c4882a881e2cdfcc15a6
Tomasz Darmetko <isinlor@gmail.com>
2019-05-06 17:11:58 +0200

    Show only one spacecraft in landing test.

--- src/LandingTest.java
+++ src/LandingTest.java
@@ -28,23 +28,20 @@ public class LandingTest {
         double probeOrbitalSpeed = titan.computeOrbitalSpeed(probeAltitude);
 
         Body orbitingProbe = new Spacecraft(
-            "Spacecraft A", new KeyboardController(),
+            "Spacecraft", new KeyboardController(),
             new Vector(titan.getRadius() + probeAltitude, 0, 0), new Vector(0, 0, Math.PI),
             new Vector(0, probeOrbitalSpeed, 0), new Vector(0, 0, 0.00),
             1, 1, new Metadata()
         );
 
-        Body landingProbe  = orbitingProbe.copy();
-        landingProbe.rename("Spacecraft B");
         bodies.addBody(orbitingProbe);
-        bodies.addBody(landingProbe);
 
         bodies.addEventListener("body crashed", (Event event) -> {
             BodyCrashedEvent crashedEvent = (BodyCrashedEvent) event;
-            System.out.println("Crashed body -- " + crashedEvent.getCrashedBody());
+            System.out.println(
+                "Landing velocity: " + crashedEvent.getCrashedBody().getVelocity().getLength() + ", " + crashedEvent.getCrashedBody()
+            );
         });
-
-        landingProbe.addVelocity(new Vector(0, -probeOrbitalSpeed/2, 0));
 
         simulation = new Simulation(bodies, steps, timeStep, stepsPerFrame, 1e4);
 
