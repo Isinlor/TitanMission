cfc66b36d1263756554fc53c7f7c9b7eb2ad6ad3
Tomasz Darmetko <isinlor@gmail.com>
2019-05-21 22:32:19 +0200

    Add spaces before units for readability.

--- src/Utilities/Units.java
+++ src/Utilities/Units.java
@@ -7,22 +7,22 @@ public class Units {
     public static String speed(double metersPerSecond) {
         double absMetersPerSecond = Math.abs(metersPerSecond);
         if(absMetersPerSecond < 1) {
-            return Utils.round(metersPerSecond * 100) + "cm/s";
+            return Utils.round(metersPerSecond * 100) + " cm/s";
         } else if(absMetersPerSecond < 1000) {
-            return Utils.round(metersPerSecond) + "m/s";
+            return Utils.round(metersPerSecond) + " m/s";
         } else {
-            return Utils.round(metersPerSecond / 1000) + "km/s";
+            return Utils.round(metersPerSecond / 1000) + " km/s";
         }
     }
     public static String distance(double meters) {
         if(meters < 1) {
-            return Utils.round(meters * 100) + "cm";
+            return Utils.round(meters * 100) + " cm";
         } else if(meters < 1000) {
-            return Utils.round(meters) + "m";
+            return Utils.round(meters) + " m";
         } else if(meters < AU) {
-            return Utils.round(meters / 1000) + "km";
+            return Utils.round(meters / 1000) + " km";
         } else {
-            return Utils.round(meters / AU) + "au";
+            return Utils.round(meters / AU) + " au";
         }
     }
 }
