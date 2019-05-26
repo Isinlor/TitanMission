2a4675193d57a95d2c66b8cce5f86cec4428c2ce
Tomasz Darmetko <isinlor@gmail.com>
2019-05-27 00:07:11 +0200

    Make wind model more forgivable

--- src/EffectSystem/Types/AtmosphereEffectSystem.java
+++ src/EffectSystem/Types/AtmosphereEffectSystem.java
@@ -13,14 +13,16 @@ public class AtmosphereEffectSystem implements EffectSystem {
 
         for (Body body: bodies.getBodies()) {
 
-            if(body.getName().equals("Titan")) continue;
+            if(bodies.getHeaviestBody() == body) continue;
 
             double angle = Math.random() * Utils.TAU;
 
             double intensity = 10;
-            if(body.getSurfaceToSurfaceDistance(bodies.getHeaviestBody()) < 100) intensity = 1;
+            if(body.getSurfaceToSurfaceDistance(bodies.getHeaviestBody()) < 1000) intensity = 1;
+            if(body.getSurfaceToSurfaceDistance(bodies.getHeaviestBody()) < 100) intensity = 0.1;
+            if(body.getSurfaceToSurfaceDistance(bodies.getHeaviestBody()) < 10) continue;
 
-            Vector wind = new Vector(Math.random(), Math.random() + 5).rotateAroundAxisZ(new Vector(), angle).product(intensity * Math.random());
+            Vector wind = new Vector(Math.random(), Math.random()).rotateAroundAxisZ(new Vector(), angle).product(intensity * Math.random());
             effects.addEffect(body, new Effect(wind, new Vector(0, 0, Math.random() / (6000 / intensity))));
 
         }
