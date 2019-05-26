8ee306786353fa62899c3e9a172ef2491356b2c0
Tomasz Darmetko <isinlor@gmail.com>
2019-05-26 15:51:49 +0200

    Add Atmosphere effect system

--- src/EffectSystem/Types/AtmosphereEffectSystem.java
+++ src/EffectSystem/Types/AtmosphereEffectSystem.java
@@ -1,4 +1,30 @@
 package EffectSystem.Types;
 
-public class AtmosphereEffectSystem {
+import EffectSystem.EffectSystem;
+import EffectSystem.*;
+import Simulation.Bodies;
+import Simulation.Body;
+import Simulation.Vector;
+import Utilities.Utils;
+
+public class AtmosphereEffectSystem implements EffectSystem {
+
+    public void collectEffects(Bodies bodies, Effects effects, double timeStep) {
+
+        for (Body body: bodies.getBodies()) {
+
+            if(body.getName().equals("Titan")) continue;
+
+            double angle = Math.random() * Utils.TAU;
+
+            double intensity = 10;
+            if(body.getSurfaceToSurfaceDistance(bodies.getHeaviestBody()) < 100) intensity = 1;
+
+            Vector wind = new Vector(Math.random(), Math.random() + 5).rotateAroundAxisZ(new Vector(), angle).product(intensity * Math.random());
+            effects.addEffect(body, new Effect(wind, new Vector(0, 0, Math.random() / (6000 / intensity))));
+
+        }
+
+    }
+
 }
