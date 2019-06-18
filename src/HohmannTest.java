4dcc6a6ed290135f9ad1282e2841cb368750d786
Tomasz Darmetko <isinlor@gmail.com>
2019-06-18 13:47:11 +0200

    Add Hohmann transfer of a spacecraft.
    10.3 km/s is the number you get when you do computations that are applicable to a satellite that wants just to raise it's orbit. interplanetary transfers are different because we are dealing with gravity of a source. that 15.0 km/s is a sum of orbital velocity + delta V required; wikipedia computes orbital velocity at 300km and that is 7.7 km/s, so the delta V that orbiting spaceship needs to expend is 15.0 - 7.7 = 7.3 km/s https://en.wikipedia.org/wiki/Hohmann_transfer_orbit#Application_to_interplanetary_travel

--- src/HohmannTest.java
+++ src/HohmannTest.java
@@ -1,8 +1,10 @@
+import ControlSystem.Controllers.NullController;
 import ODESolvers.LeapfrogODE;
 import Simulation.Bodies;
 import Simulation.Body;
 import Simulation.Constants;
 import Simulation.Vector;
+import Simulation.*;
 import Utilities.FileSystem;
 import Utilities.Tuple;
 import Utilities.Utils;
@@ -17,15 +19,14 @@ public class HohmannTest {
     private static final long steps = (long)(600*24*60*60 / timeStep);
     private static final long stepsPerFrame = (long)(24*60*60 / timeStep); // around 1 day
 
-    private static String resourcesPath;
-
     /**
      * Some simple test.
      */
     public static void main(String[] args) throws Exception {
 
-        resourcesPath = args[0];
         Bodies bodies = Bodies.unserialize(FileSystem.tryLoadResource("planets.txt"));
+
+        Body sun = bodies.getBody("Sun");
         Body earth = bodies.getBody("Earth");
         Body saturn = bodies.getBody("Saturn");
 
@@ -43,8 +44,17 @@ public class HohmannTest {
 
         }
 
-        earth.setMass(1);
-        earth.addVelocity(earth.getVelocity().unitVector().product(10.28 * 1000));
+
+        Spacecraft spacecraft = new Spacecraft("Spacecraft", "Saturn", new NullController());
+        bodies.addBody(spacecraft);
+
+//        earth.setMass(1);
+//        earth.addVelocity(earth.getVelocity().unitVector().product(10.3 * 1000));
+
+        Vector directionFromEarthToSun = earth.getRelativePosition(sun).unitVector();
+
+        spacecraft.addVelocity(earth.getVelocity().sum(earth.getVelocity().unitVector().product(15 * 1000)));
+        spacecraft.addPosition(earth.getPosition().sum(directionFromEarthToSun.product(10000 * 1000)));
 
         simulation = new Simulation(bodies, steps, timeStep, stepsPerFrame, 0.4e10);
 
