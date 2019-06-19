97dcb69a82363ee97622f3466cacbdf8f2a3be43
Tomasz Darmetko <isinlor@gmail.com>
2019-06-19 17:32:38 +0200

    Improve HohmannTest
    Use mini setup.
    Add Titan with atmosphere.
    Add controllers.

--- src/HohmannTest.java
+++ src/HohmannTest.java
@@ -1,22 +1,24 @@
-import ControlSystem.Controllers.NullController;
+import ControlSystem.Controller;
+import ControlSystem.Controllers.*;
 import ODESolvers.LeapfrogODE;
 import Simulation.Bodies;
 import Simulation.Body;
-import Simulation.Constants;
-import Simulation.Vector;
+import Simulation.SolarSystem.Titan;
 import Simulation.*;
 import Utilities.FileSystem;
-import Utilities.Tuple;
+import Utilities.Units;
 import Utilities.Utils;
 import Visualisation.Simulation;
+
+import java.util.function.Function;
 
 @SuppressWarnings("Duplicates")
 public class HohmannTest {
 
     private static Simulation simulation;
 
-    private static final double timeStep = 200*60.0; // in s
-    private static final long steps = (long)(600*24*60*60 / timeStep);
+    private static final double timeStep = 60.0; // in s
+    private static final long steps = (long)(10000*24*60*60 / timeStep);
     private static final long stepsPerFrame = (long)(24*60*60 / timeStep); // around 1 day
 
     /**
@@ -24,11 +26,16 @@ public class HohmannTest {
      */
     public static void main(String[] args) throws Exception {
 
-        Bodies bodies = Bodies.unserialize(FileSystem.tryLoadResource("planets.txt"));
+        Bodies bodies = Bodies.unserialize(FileSystem.tryLoadResource("planets-mini.txt"));
 
         Body sun = bodies.getBody("Sun");
         Body earth = bodies.getBody("Earth");
         Body saturn = bodies.getBody("Saturn");
+        Body titan = bodies.getBody("Titan");
+        bodies.removeBody("Titan");
+        bodies.addBody(new Titan());
+        bodies.getBody("Titan").addPosition(titan.getPosition());
+        bodies.getBody("Titan").addVelocity(titan.getVelocity());
 
         for (int i = 0; i < 100; i++) {
             new LeapfrogODE().iterate(bodies, timeStep);
@@ -38,23 +45,43 @@ public class HohmannTest {
             double relativeAngle = Math.toDegrees(Utils.getSignedDistanceBetweenAngles(earthAngle, saturnAngle));
             relativeAngle = Math.abs(relativeAngle);
 
-            System.out.println(relativeAngle);
-
             if(relativeAngle < 106.5) break;
-
         }
 
+        Function<Spacecraft, Double> function = (Spacecraft spacecraft) -> {
+            double altitude = spacecraft.getSurfaceToSurfaceDistance(spacecraft.getTarget());
+            if(altitude < 500000) return 0.0;
+            return  altitude < Units.AU ? 1. : 0.;
+        };
 
-        Spacecraft spacecraft = new Spacecraft("Spacecraft", "Saturn", new NullController());
+
+        Controller controller = new CompositeController(
+            WeightedController.createStartAtAltitudeController(
+              new SuicideBurnController(1000),
+              10000
+            ),
+            new WeightedController(
+                new DestinationController(0.1),
+                function,
+                function
+            )
+        );
+
+        Spacecraft spacecraft = new Spacecraft("Spacecraft", "Titan", controller);
         bodies.addBody(spacecraft);
 
 //        earth.setMass(1);
 //        earth.addVelocity(earth.getVelocity().unitVector().product(10.3 * 1000));
 
-        Vector directionFromEarthToSun = earth.getRelativePosition(sun).unitVector();
+        double orbitHeight = 300 * 1000;
 
-        spacecraft.addVelocity(earth.getVelocity().sum(earth.getVelocity().unitVector().product(15 * 1000)));
-        spacecraft.addPosition(earth.getPosition().sum(directionFromEarthToSun.product(10000 * 1000)));
+        Vector directionFromEarthToSun = earth.getRelativePosition(sun).unitVector();
+        Vector earthVelocityDirection = earth.getVelocity().unitVector();
+
+        spacecraft.addVelocity(earth.getVelocity().sum(earthVelocityDirection.product(15.5 * 1000)));
+        spacecraft.addPosition(
+            earth.getPosition().sum(directionFromEarthToSun.product(earth.getRadius() + orbitHeight))
+        );
 
         simulation = new Simulation(bodies, steps, timeStep, stepsPerFrame, 0.4e10);
 
