60a716d417235d410540948a7372bfb8d119b1d6
Tomasz Darmetko <isinlor@gmail.com>
2019-06-24 22:23:45 +0200

    Fix AthmosphereEffectSystem.

--- src/EffectSystem/Types/AtmosphereEffectSystem.java
+++ src/EffectSystem/Types/AtmosphereEffectSystem.java
@@ -7,6 +7,8 @@ import Simulation.Body;
 import Simulation.Vector;
 import Utilities.Utils;
 
+import java.util.Random;
+
 public class AtmosphereEffectSystem implements EffectSystem {
 
     public void collectEffects(Bodies bodies, Effects effects, double timeStep) {
@@ -17,12 +19,15 @@ public class AtmosphereEffectSystem implements EffectSystem {
 
             double angle = Math.random() * Utils.TAU;
 
-            double intensity = 1200;
-            if(body.getSurfaceToSurfaceDistance(bodies.getHeaviestBody()) < 1000) intensity = 100;
-            if(body.getSurfaceToSurfaceDistance(bodies.getHeaviestBody()) < 100) intensity = 1;
+            Random random = new Random();
+
+            double intensity = 10;
+            if(body.getSurfaceToSurfaceDistance(bodies.getHeaviestBody()) < 10000) intensity = 5;
+            if(body.getSurfaceToSurfaceDistance(bodies.getHeaviestBody()) < 1000) intensity = 1;
+            if(body.getSurfaceToSurfaceDistance(bodies.getHeaviestBody()) < 100) intensity = 0.3;
             if(body.getSurfaceToSurfaceDistance(bodies.getHeaviestBody()) < 10) continue;
 
-            Vector wind = new Vector(Math.random() - 5, Math.random()).rotateAroundAxisZ(new Vector(), angle).product(intensity * Math.random());
+            Vector wind = new Vector(random.nextGaussian(), random.nextGaussian()).rotateAroundAxisZ(new Vector(), angle).product(intensity * Math.random());
             effects.addEffect(body, new Effect(wind, new Vector(0, 0, Math.random() / (6000 / intensity))));
 
         }
